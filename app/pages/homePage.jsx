78be99dec660e8939d7ed852ab5abf65256e08e9
Michael Mejia <mejia.mora@gmail.com>
2018-11-08 18:44:19 -0600

    Build

--- app/pages/homePage.jsx
+++ app/pages/homePage.jsx
@@ -3,6 +3,7 @@ import NavbarComponent from '../components/navbar.jsx'
 import BodyContent from '../components/body.jsx'
 import FooterContent from '../components/footer.jsx'
 import JumbotronComponent from '../components/jumbotron.jsx'
+
 export default class HomePage extends React.Component {
   constructor (props) {
     super(props)
